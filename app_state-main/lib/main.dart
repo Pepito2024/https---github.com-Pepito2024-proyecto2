cc20f715fc7987ef65c10f0cb4079f058ea4ed47
Pepito2024 <021370b@utea.edu.pe>
2023-09-15 18:11:21 -0500

    todo bien 2000

---
+++ app_state-main/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:app_state/home_page.dart';
+import 'package:app_state/test1_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Test1Page(),
+    );
+  }
+}
+////9999999999
